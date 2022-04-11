ddb7e88ab9f773720d540e0d132ecaa09e40005f
Dustin Hurtt <dustin_hurtt@outlook.com>
2022-04-11 12:44:12 -0400

    done

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function(a, b) {
+      return a - b;
+    });
+    this.length = this.items.length;
 
-  get(pos) {}
+  }
 
-  max() {}
+  get(pos) {
+    if (pos > this.items.length - 1) {
+      throw new Error('OutOfBounds');
+    } else {
+    return this.items.indexOf(pos)}
+  }
 
-  min() {}
+  max() {
+    if (this.items.length <= 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.max(...this.items)
+    }
+  }
 
-  sum() {}
+  min() {
+    if (this.items.length <= 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.min(...this.items)
+    }
+  }
 
-  avg() {}
+  sum() {
+    return this.items.reduce((a, b) => a + b, 0)
+  }
+
+  avg() {
+    if (this.items.length <= 0) {
+    throw new Error('EmptySortedList');
+  } else {
+    return ((this.items.reduce((a, b) => a + b, 0))/this.length)
+    }
+  }
 }
 
 module.exports = SortedList;
